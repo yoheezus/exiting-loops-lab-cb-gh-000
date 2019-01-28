f027ed4abb41dd80938011ac8c6c20c14b31baea
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-28 17:58:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function breakOut(array, changeValue, stopValue) {
-    for (let i = 0; i < array; i++) {
+    for (let i = 0; i < array.length; i++) {
         if (array[i] === stopValue) {
             break;
         }
