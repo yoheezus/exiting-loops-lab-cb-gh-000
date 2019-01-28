03190548063c3abf841b518cfa3b5bed68321af7
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-28 18:08:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function keepGoing(array, changeValue, skipValue) {
 function findBy(array, findFn) {
     for (let i = 0; i < array.length; i++) {
         if (array[i] !== findFn) {
-            return findFn
+            return array[i]
         }
     }
     return null
